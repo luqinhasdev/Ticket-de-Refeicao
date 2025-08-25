7796ace7e1a5fcea3af0d246d8b21b218ff1da27
luqinhas <gaminggclark@gmail.com>
2025-08-25 16:16:51 -0300

    ADICIONANDO TELAS PROS INTEGRANTES MODIFICAREM DEPOIS

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TESTE</Text>
+      <Text>TESsdasdTE</Text>
       <StatusBar style="auto" />
     </View>
   );
