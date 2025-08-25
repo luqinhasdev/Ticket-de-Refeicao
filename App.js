8a8609dd0b8bb661ee50b318eb03615077c78df0
LUCAS AITA WEIDMANN <lucas_weidmann@estudante.sesisenai.org.br>
2025-08-25 16:00:22 -0300

    ADICIONANDO README E REMOVENDO ARQUIVOS INÚTEIS

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fist commit nessa porra</Text>
+      <Text>TESTE</Text>
       <StatusBar style="auto" />
     </View>
   );
